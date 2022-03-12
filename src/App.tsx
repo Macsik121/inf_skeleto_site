0c04ac260428ed51ec48a04c6d303a269aadaeb4
Macsik121 <hiomimipad@gmail.com>
2022-03-12 14:34:16 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import globals from "../globals";
 import "./styles/style.css";
-// if (!globals.production)
-import"/src/images/jotaro.webp";import"/src/images/bg.gif";import"/src/images/video.mp4";import"/src/images/video.gif";
+import"./images/jotaro.webp";import"./images/bg.gif";import"./images/video.mp4";import"./images/video.gif";
 import Routing from "./Routing";
 
 const App: FC = () => {
